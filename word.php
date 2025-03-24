5e86d04b5e135565dffcf0866d62ec2f8fb4ab53
Hussain <hussainshafiq51@gmail.com>
2025-03-24 13:10:24 +0500

    word

--- word.php
+++ word.php
@@ -10,7 +10,7 @@
     $paragraph="Technology has become an integral part of modern life, influencing various aspects of daily activities and industries. Its rapid advancement has transformed communication, education, and healthcare, among others, enabling greater efficiency and connectivity. As innovations continue to emerge, the impact of technology on society will likely expand, presenting both opportunities and challenges that require careful consideration.";
 
     echo"<p>".$paragraph."<p>" ;
-    //geting input from user
+    //geting input from user 
     echo"<form method='post'>";
     echo "<label for='wordsearch'> Enter a word to search:</label>";
     echo"<input type='text' name='wordsearch' id='wordsearch'>";
@@ -21,7 +21,7 @@
     $wordsearch=$_POST['wordsearch'];
     //using srtripos to find the postion of the word
     $position=stripos($paragraph, $wordsearch);
-    
+    //check if the word exists
     if($position !== false){
         
         echo "<p>The word '" . htmlspecialchars($wordsearch) . "' was found at position " . $position . ".</p>";        
