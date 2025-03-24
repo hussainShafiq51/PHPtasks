978604634a3e279e6b7fd1511f33840fab658629
Hussain <hussainshafiq51@gmail.com>
2025-03-24 12:23:12 +0500

    word to find

---
+++ word.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>word search</title>
+</head>
+<body>
+    <?php 
+    $paragraph="Technology has become an integral part of modern life, influencing various aspects of daily activities and industries. Its rapid advancement has transformed communication, education, and healthcare, among others, enabling greater efficiency and connectivity. As innovations continue to emerge, the impact of technology on society will likely expand, presenting both opportunities and challenges that require careful consideration.";
+
+    echo"<p>".$paragraph."<p>" ;
+    
+    echo"<input type='text'> name='wordsearch' id='wordsearch'";
+    echo"<input type='submit' value='search'>";
+
+
+    
+    
+    ?>
+    
+</body>
+</html>
